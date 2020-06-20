9706db52764c685e136c79e557e1612ec7504643
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-06-20 12:20:29 -0400

    chore: remove unneeded logging

--- android/NotificationHelper.java
+++ android/NotificationHelper.java
@@ -11,13 +11,10 @@ import android.graphics.drawable.Icon;
 public class NotificationHelper {
     private final static String tag = "NotificationHelper";
     public static void newChannel(Context ctx, int importance, String channelID, String name, String description) {
-        Log.w(tag,String.format("newChannel invoked"));
         NotificationChannel channel = new NotificationChannel(channelID, name, importance);
-    	Log.e(tag,String.format("channel: %s",channel));
         channel.setDescription(description);
 
         NotificationManager notificationManager = ctx.getSystemService(NotificationManager.class);
-    	Log.e(tag,String.format("manager: %s",notificationManager));
         notificationManager.createNotificationChannel(channel);
     }
     public static void sendNotification(Context ctx, String channelID, int notificationID, String title, String text) {
