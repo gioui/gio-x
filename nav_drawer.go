61432161b6a3b6e2f008c703348bba34a8f6435a
Chris Waldon <christopher.waldon.dev@gmail.com>
2020-07-18 16:21:34 -0400

    feat: use material.Clickable for nav items
    This prevents needing to duplicate the inking logic that was
    already implemented in package material.

--- nav_drawer.go
+++ nav_drawer.go
@@ -3,8 +3,6 @@ package materials
 import (
 	"image"
 	"image/color"
-	"log"
-	"math"
 	"time"
 
 	"gioui.org/f32"
@@ -93,7 +91,6 @@ func (n *renderNavItem) Layout(gtx layout.Context) layout.Dimensions {
 			}
 		}
 	}
-	n.Clickable.Layout(gtx)
 	defer op.Push(gtx.Ops).Pop()
 	pointer.PassOp{Pass: true}.Add(gtx.Ops)
 	pointer.Rect(image.Rectangle{
@@ -109,14 +106,12 @@ func (n *renderNavItem) Layout(gtx layout.Context) layout.Dimensions {
 		Left:   unit.Dp(8),
 		Right:  unit.Dp(8),
 	}.Layout(gtx, func(gtx C) D {
-		return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-			layout.Rigid(func(gtx C) D {
-				return layout.Stack{}.Layout(gtx,
-					layout.Expanded(n.layoutBackground),
-					layout.Stacked(n.layoutContent),
-				)
-			}),
-		)
+		return material.Clickable(gtx, &n.Clickable, func(gtx C) D {
+			return layout.Stack{}.Layout(gtx,
+				layout.Expanded(n.layoutBackground),
+				layout.Stacked(n.layoutContent),
+			)
+		})
 	})
 }
 
@@ -127,7 +122,6 @@ func (n *renderNavItem) layoutContent(gtx layout.Context) layout.Dimensions {
 	}
 	return layout.Inset{Left: unit.Dp(8)}.Layout(gtx, func(gtx C) D {
 		return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
-
 			layout.Rigid(func(gtx C) D {
 				if n.Icon == nil {
 					return layout.Dimensions{}
@@ -181,117 +175,7 @@ func (n *renderNavItem) layoutBackground(gtx layout.Context) layout.Dimensions {
 		SW: rr,
 	}.Add(gtx.Ops)
 	paintRect(gtx, gtx.Constraints.Max, fill)
-	for _, click := range n.Clickable.History() {
-		n.drawInk(gtx, click)
-	}
 	return layout.Dimensions{Size: gtx.Constraints.Max}
-}
-
-// adapted from https://git.sr.ht/~eliasnaur/gio/tree/773939fe1dd10b3ac5f937a7f9993045a91e23a7/widget/material/button.go#L189
-func (n *renderNavItem) drawInk(gtx layout.Context, c widget.Press) {
-	// duration is the number of seconds for the
-	// completed animation: expand while fading in, then
-	// out.
-	const (
-		expandDuration = float32(0.5)
-		fadeDuration   = float32(0.9)
-	)
-
-	now := gtx.Now
-
-	t := float32(now.Sub(c.Start).Seconds())
-
-	end := c.End
-	if end.IsZero() {
-		// If the press hasn't ended, don't fade-out.
-		end = now
-	}
-
-	endt := float32(end.Sub(c.Start).Seconds())
-
-	// Compute the fade-in/out position in [0;1].
-	var alphat float32
-	{
-		var haste float32
-		if c.Cancelled {
-			// If the press was cancelled before the inkwell
-			// was fully faded in, fast forward the animation
-			// to match the fade-out.
-			if h := 0.5 - endt/fadeDuration; h > 0 {
-				haste = h
-			}
-		}
-		// Fade in.
-		half1 := t/fadeDuration + haste
-		if half1 > 0.5 {
-			half1 = 0.5
-		}
-
-		// Fade out.
-		half2 := float32(now.Sub(end).Seconds())
-		half2 /= fadeDuration
-		half2 += haste
-		if half2 > 0.5 {
-			// Too old.
-			return
-		}
-
-		alphat = half1 + half2
-	}
-
-	// Compute the expand position in [0;1].
-	sizet := t
-	if c.Cancelled {
-		// Freeze expansion of cancelled presses.
-		sizet = endt
-	}
-	sizet /= expandDuration
-
-	// Animate only ended presses, and presses that are fading in.
-	if !c.End.IsZero() || sizet <= 1.0 {
-		op.InvalidateOp{}.Add(gtx.Ops)
-	}
-
-	if sizet > 1.0 {
-		sizet = 1.0
-	}
-
-	if alphat > .5 {
-		// Start fadeout after half the animation.
-		alphat = 1.0 - alphat
-	}
-	// Twice the speed to attain fully faded in at 0.5.
-	t2 := alphat * 2
-	// Beziér ease-in curve.
-	alphaBezier := t2 * t2 * (3.0 - 2.0*t2)
-	sizeBezier := sizet * sizet * (3.0 - 2.0*sizet)
-	size := float32(gtx.Constraints.Min.X)
-	if h := float32(gtx.Constraints.Min.Y); h > size {
-		size = h
-	}
-	// Cover the entire constraints min rectangle.
-	size *= 2 * float32(math.Sqrt(2))
-	// Apply curve values to size and color.
-	size *= sizeBezier
-	alpha := 0.7 * alphaBezier
-	const col = 0.8
-	ba, bc := byte(alpha*0xff), byte(alpha*col*0xff)
-	defer op.Push(gtx.Ops).Pop()
-	ink := paint.ColorOp{Color: color.RGBA{A: ba, R: bc, G: bc, B: bc}}
-	ink.Add(gtx.Ops)
-	rr := size * .5
-	op.Offset(c.Position.Add(f32.Point{
-		X: -rr,
-		Y: -rr,
-	})).Add(gtx.Ops)
-	clip.RRect{
-		Rect: f32.Rectangle{Max: f32.Point{
-			X: float32(size),
-			Y: float32(size),
-		}},
-		NE: rr, NW: rr, SE: rr, SW: rr,
-	}.Add(gtx.Ops)
-	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{X: float32(size), Y: float32(size)}}}.Add(gtx.Ops)
 }
 
 // ModalNavDrawer implements the Material Design Modal Navigation Drawer
@@ -400,7 +284,6 @@ func (m *ModalNavDrawer) updateDragState(gtx layout.Context) {
 func (m *ModalNavDrawer) drawerTransform(gtx layout.Context) op.TransformOp {
 	revealed := -1 + m.Revealed(gtx)
 	finalOffset := revealed*(float32(m.sheetWidth(gtx))) - m.dragOffset
-	log.Printf("revealed: %v final: %v drag: %v", revealed, finalOffset, m.dragOffset)
 	return op.Offset(f32.Point{X: finalOffset})
 }
 
