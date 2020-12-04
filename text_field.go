667ae091b6fdf0e94d1058f41c932b562a36998b
Jack Mordaunt <jackmordaunt@gmail.com>
2020-12-03 21:08:11 -0500

    Signed-off-by: Jack Mordaunt <jackmordaunt@gmail.com>

--- text_field.go
+++ text_field.go
@@ -437,15 +437,3 @@ func (h *Hoverable) update(gtx C) {
 		}
 	}
 }
-
-type Rect struct {
-	Color color.NRGBA
-	Size  image.Point
-	Radii float32
-}
-
-func (r Rect) Layout(gtx C) D {
-	paint.FillShape(gtx.Ops, r.Color, clip.UniformRRect(f32.Rectangle{Max: layout.FPt(r.Size)}, r.Radii).Op(gtx.Ops))
-	return layout.Dimensions{Size: image.Pt(r.Size.X, r.Size.Y)}
-
-}
