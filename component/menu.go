6348dfd76fc237e11420709545661bca6e82e43c
Chris Waldon <christopher.waldon.dev@gmail.com>
2021-03-07 15:13:20 -0500

    feat: implement simple menu and card types
    This commit adds a Card implementation that can produce a surface
    with a drop shadow. The card itself does not yet support rounded corners.
    
    This Card type is then used as the foundation layer for a simple
    Menu type that can present a list of widgets on a card.
    
    This implementation is nowhere near complete, and has several problems:
    - the drop shadow has artifacts at the corners where the shadow doesn't
      fade all the way to the background color.
    - the drop shadow has small seams between the four different panels that
      compose it.
    - the card can't itself have rounded corners without exposing an area with
      no shadow underneath.
    - the MenuState currently holds the layout funcs for the menu options. I'm
      not actually certain that this is a good design, and I may want to move
      those to be provided at layout time.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

---
+++ component/menu.go
@@ -0,0 +1,171 @@
+package component
+
+import (
+	"image/color"
+
+	"gioui.org/f32"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+)
+
+// CardStyle defines the visual aspects of a material design surface
+// with (optionally) rounded corners and a drop shadow.
+type CardStyle struct {
+	*material.Theme
+	layout.Inset
+	Radius unit.Value
+	Shadow color.NRGBA
+}
+
+func Card(th *material.Theme) CardStyle {
+	defaultShadowSize := unit.Dp(20)
+	return CardStyle{
+		Theme:  th,
+		Inset:  layout.UniformInset(defaultShadowSize),
+		Radius: defaultShadowSize,
+		Shadow: color.NRGBA{A: 155},
+	}
+}
+
+func (c CardStyle) Layout(gtx C, w layout.Widget) D {
+	macro := op.Record(gtx.Ops)
+	dims := w(gtx)
+	call := macro.Stop()
+
+	finalDims := c.Inset.Layout(gtx, func(gtx C) D {
+		call.Add(gtx.Ops)
+		return dims
+	})
+	defer op.Save(gtx.Ops).Load()
+	clip.UniformRRect(f32.Rectangle{Max: layout.FPt(finalDims.Size)}, float32(gtx.Px(c.Radius))).Add(gtx.Ops)
+
+	left := (gtx.Px(c.Inset.Left))
+	top := (gtx.Px(c.Inset.Top))
+	right := (gtx.Px(c.Inset.Right))
+	bottom := (gtx.Px(c.Inset.Bottom))
+
+	pathFrom := func(gtx C, points ...f32.Point) clip.PathSpec {
+		p := clip.Path{}
+		p.Begin(gtx.Ops)
+		p.MoveTo(points[0])
+		for _, point := range points[1:] {
+			p.LineTo(point)
+		}
+		p.Close()
+		return p.End()
+	}
+	topSpec := pathFrom(gtx,
+		f32.Point{},
+		f32.Point{
+			X: float32(left + dims.Size.X + right),
+		},
+		f32.Point{
+			X: float32(left + dims.Size.X),
+			Y: float32(top),
+		},
+		f32.Point{
+			X: float32(left),
+			Y: float32(top),
+		},
+	)
+	leftSpec := pathFrom(gtx,
+		f32.Point{},
+		f32.Point{X: float32(left), Y: float32(top)},
+		f32.Point{X: float32(left), Y: float32(top + dims.Size.Y)},
+		f32.Point{Y: float32(top + dims.Size.Y + bottom)},
+	)
+	rightSpec := pathFrom(gtx,
+		f32.Point{X: float32(left + right + dims.Size.X)},
+		f32.Point{X: float32(left + right + dims.Size.X),
+			Y: float32(top + bottom + dims.Size.Y)},
+		f32.Point{X: float32(left + dims.Size.X),
+			Y: float32(top + dims.Size.Y)},
+		f32.Point{X: float32(left + dims.Size.X),
+			Y: float32(top)},
+	)
+	bottomSpec := pathFrom(gtx,
+		f32.Point{Y: float32(top + bottom + dims.Size.Y)},
+		f32.Point{X: float32(left + right + dims.Size.X),
+			Y: float32(top + bottom + dims.Size.Y)},
+		f32.Point{X: float32(left + dims.Size.X),
+			Y: float32(top + dims.Size.Y)},
+		f32.Point{X: float32(left),
+			Y: float32(top + dims.Size.Y)},
+	)
+
+	topGrad := paint.LinearGradientOp{
+		Stop2:  f32.Point{Y: float32(top)},
+		Color2: c.Shadow,
+	}
+	leftGrad := paint.LinearGradientOp{
+		Stop2:  f32.Point{X: float32(left)},
+		Color2: c.Shadow,
+	}
+	rightGrad := paint.LinearGradientOp{
+		Stop1:  f32.Point{X: float32(left + right + dims.Size.X)},
+		Stop2:  f32.Point{X: float32(left + dims.Size.X)},
+		Color2: c.Shadow,
+	}
+	bottomGrad := paint.LinearGradientOp{
+		Stop1:  f32.Point{Y: float32(top + bottom + dims.Size.Y)},
+		Stop2:  f32.Point{Y: float32(top + dims.Size.Y)},
+		Color2: c.Shadow,
+	}
+	apply := func(gtx C, grad paint.LinearGradientOp, p clip.PathSpec) {
+		defer op.Save(gtx.Ops).Load()
+		clip.Outline{Path: p}.Op().Add(gtx.Ops)
+		grad.Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
+	}
+	apply(gtx, topGrad, topSpec)
+	apply(gtx, leftGrad, leftSpec)
+	apply(gtx, rightGrad, rightSpec)
+	apply(gtx, bottomGrad, bottomSpec)
+	return finalDims
+}
+
+// MenuState holds the state of a menu material design component
+// across frames.
+type MenuState struct {
+	OptionList layout.List
+	Options    []func(gtx C) D
+}
+
+// MenuStyle defines the presentation of a material design menu component.
+type MenuStyle struct {
+	*MenuState
+	*material.Theme
+	layout.Inset
+	CardStyle
+}
+
+func Menu(th *material.Theme, state *MenuState) MenuStyle {
+	return MenuStyle{
+		Theme:     th,
+		MenuState: state,
+		CardStyle: Card(th),
+	}
+}
+
+func (m MenuStyle) Layout(gtx C) D {
+	m.OptionList.Axis = layout.Vertical
+	return m.CardStyle.Layout(gtx, func(gtx C) D {
+		return layout.Stack{}.Layout(gtx,
+			layout.Expanded(func(gtx C) D {
+				return Rect{
+					Color: m.Theme.Bg,
+					Size:  gtx.Constraints.Min,
+				}.Layout(gtx)
+			}),
+			layout.Stacked(func(gtx C) D {
+				return m.OptionList.Layout(gtx, len(m.Options), func(gtx C, index int) D {
+					return m.Options[index](gtx)
+				})
+			}),
+		)
+	})
+}
---
+++ component/menudemo/main.go
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+package main
+
+// A simple Gio program. See https://gioui.org for more information.
+
+import (
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/paint"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+	"gioui.org/x/component"
+
+	"gioui.org/font/gofont"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		if err := loop(w); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+type (
+	C = layout.Context
+	D = layout.Dimensions
+)
+
+func loop(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+	inset := layout.UniformInset(unit.Dp(8))
+	menu := component.MenuState{
+		Options: []func(gtx layout.Context) layout.Dimensions{
+			func(gtx layout.Context) layout.Dimensions {
+				return inset.Layout(gtx, func(gtx C) D {
+					return material.Body1(th, "Foo").Layout(gtx)
+				})
+			},
+			func(gtx layout.Context) layout.Dimensions {
+				return inset.Layout(gtx, func(gtx C) D {
+					return material.Body1(th, "Bar").Layout(gtx)
+				})
+			},
+			func(gtx layout.Context) layout.Dimensions {
+				return inset.Layout(gtx, func(gtx C) D {
+					return material.Body1(th, "Baz").Layout(gtx)
+				})
+			},
+		},
+	}
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			paint.Fill(gtx.Ops, color.NRGBA{R: 200, G: 200, B: 200, A: 255})
+			layout.Center.Layout(gtx, func(gtx C) D {
+				return component.Menu(th, &menu).Layout(gtx)
+			})
+			e.Frame(gtx.Ops)
+		}
+	}
+}
