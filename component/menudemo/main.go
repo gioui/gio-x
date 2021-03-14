f986749428b5f32cef07b7c18735394bada05343
Chris Waldon <christopher.waldon.dev@gmail.com>
2021-03-14 15:48:01 -0400

    feat: prototype right click context menu in demo program
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- component/menudemo/main.go
+++ component/menudemo/main.go
@@ -11,11 +11,14 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/f32"
+	"gioui.org/io/pointer"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"gioui.org/x/component"
 
@@ -38,30 +41,119 @@ type (
 	D = layout.Dimensions
 )
 
+type RightClickArea struct {
+	position f32.Point
+	dims     D
+	active   bool
+}
+
+func (r *RightClickArea) Layout(gtx C, w layout.Widget) D {
+	pointer.Rect(image.Rectangle{Max: gtx.Constraints.Min}).Add(gtx.Ops)
+	pointer.PassOp{Pass: true}.Add(gtx.Ops)
+	pointer.InputOp{
+		Tag:   r,
+		Grab:  false,
+		Types: pointer.Press | pointer.Release,
+	}.Add(gtx.Ops)
+	for _, e := range gtx.Events(r) {
+		e, ok := e.(pointer.Event)
+		if !ok {
+			continue
+		}
+		if r.active {
+			// Check whether we should dismiss menu.
+			if e.Buttons.Contain(pointer.ButtonPrimary) {
+				clickPos := e.Position.Sub(r.position)
+				if !clickPos.In(f32.Rectangle{Max: layout.FPt(r.dims.Size)}) {
+					r.Dismiss()
+				}
+			}
+		}
+		if e.Buttons.Contain(pointer.ButtonSecondary) {
+			r.active = true
+			r.position = e.Position
+		}
+	}
+	dims := D{Size: gtx.Constraints.Min}
+
+	if !r.active {
+		return dims
+	}
+
+	for _, e := range gtx.Events(&r.active) {
+		e, ok := e.(pointer.Event)
+		if !ok {
+			continue
+		}
+		if e.Type == pointer.Release {
+			r.Dismiss()
+		}
+	}
+
+	defer op.Save(gtx.Ops).Load()
+	macro := op.Record(gtx.Ops)
+	r.dims = w(gtx)
+	call := macro.Stop()
+
+	if int(r.position.X)+r.dims.Size.X > gtx.Constraints.Max.X {
+		r.position.X = float32(gtx.Constraints.Max.X - r.dims.Size.X)
+	}
+	if int(r.position.Y)+r.dims.Size.Y > gtx.Constraints.Max.Y {
+		r.position.Y = float32(gtx.Constraints.Max.Y - r.dims.Size.Y)
+	}
+	macro2 := op.Record(gtx.Ops)
+	op.Offset(r.position).Add(gtx.Ops)
+	call.Add(gtx.Ops)
+	pointer.PassOp{Pass: true}.Add(gtx.Ops)
+	pointer.Rect(image.Rectangle{Max: r.dims.Size}).Add(gtx.Ops)
+	pointer.InputOp{
+		Tag:   &r.active,
+		Grab:  false,
+		Types: pointer.Release,
+	}.Add(gtx.Ops)
+	call2 := macro2.Stop()
+	op.Defer(gtx.Ops, call2)
+	return dims
+}
+
+func (r *RightClickArea) Dismiss() {
+	r.active = false
+}
+
 func loop(w *app.Window) error {
 	th := material.NewTheme(gofont.Collection())
-	var ops op.Ops
+	var (
+		ops     op.Ops
+		a, b, c widget.Clickable
+	)
 	inset := layout.UniformInset(unit.Dp(8))
 	menu := component.MenuState{
 		Options: []func(gtx layout.Context) layout.Dimensions{
 			func(gtx layout.Context) layout.Dimensions {
-				return inset.Layout(gtx, func(gtx C) D {
-					return material.Body1(th, "Foo").Layout(gtx)
+				return material.Clickable(gtx, &a, func(gtx C) D {
+					return inset.Layout(gtx, func(gtx C) D {
+						return material.Body1(th, "Foo").Layout(gtx)
+					})
 				})
 			},
 			func(gtx layout.Context) layout.Dimensions {
-				return inset.Layout(gtx, func(gtx C) D {
-					return material.Body1(th, "Bar").Layout(gtx)
+				return material.Clickable(gtx, &b, func(gtx C) D {
+					return inset.Layout(gtx, func(gtx C) D {
+						return material.Body1(th, "Bar").Layout(gtx)
+					})
 				})
 			},
 			func(gtx layout.Context) layout.Dimensions {
-				return inset.Layout(gtx, func(gtx C) D {
-					return material.Body1(th, "Baz").Layout(gtx)
+				return material.Clickable(gtx, &c, func(gtx C) D {
+					return inset.Layout(gtx, func(gtx C) D {
+						return material.Body1(th, "Baz").Layout(gtx)
+					})
 				})
 			},
 		},
 	}
 	var shadows layout.List
+	var area RightClickArea
 	for {
 		e := <-w.Events()
 		switch e := e.(type) {
@@ -70,7 +162,18 @@ func loop(w *app.Window) error {
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
 			paint.Fill(gtx.Ops, color.NRGBA{R: 200, G: 200, B: 200, A: 255})
-			layout.Center.Layout(gtx, func(gtx C) D {
+			gtx.Constraints = layout.Exact(gtx.Constraints.Max)
+			if a.Clicked() {
+				log.Println("A")
+			}
+			if b.Clicked() {
+				log.Println("B")
+			}
+			if c.Clicked() {
+				log.Println("C")
+			}
+			area.Layout(gtx, func(gtx C) D {
+				gtx.Constraints.Min = image.Point{}
 				return component.Menu(th, &menu).Layout(gtx)
 			})
 			layout.N.Layout(gtx, func(gtx C) D {
