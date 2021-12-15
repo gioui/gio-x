807e1550f1dae43f4d60e31493e83ca4adb4110f
Inkeliz <inkeliz@inkeliz.com>
2021-12-15 16:44:02 +0000

    explorer: add the new API exported symbols and definitions

--- explorer/explorer.go
+++ explorer/explorer.go
@@ -3,9 +3,15 @@
 package explorer
 
 import (
+	"crypto/rand"
 	"errors"
+	"gioui.org/app"
 	"gioui.org/io/event"
 	"io"
+	"math"
+	"math/big"
+	"runtime"
+	"sync"
 )
 
 var (
@@ -15,6 +21,50 @@ var (
 	// ErrNotAvailable is return when the current OS isn't supported.
 	ErrNotAvailable = errors.New("current OS not supported")
 )
+
+type result struct {
+	file  interface{}
+	error error
+}
+
+type Explorer struct {
+	id    int32
+	mutex sync.Mutex
+
+	// explorer holds OS-Specific content, it varies for each OS.
+	*explorer
+}
+
+// active holds all explorer currently active, that may necessary for callback functions.
+//
+// Some OSes (Android, iOS, macOS) may call Golang exported functions as callback, but we need
+// someway to link that callback with the respective explorer, in order to give them a response.
+//
+// In that case, a construction like `callback(..., id int32)` is used. Then, it's possible to get the explorer
+// by lookup the active using the callback id.
+//
+// To avoid hold dead/unnecessary explorer, the active will be removed using `runtime.SetFinalizer` on the related
+// Explorer.
+var active = sync.Map{} // map[int32]*explorer
+
+// NewExplorer creates a new Explorer for the given *app.Window.
+// The given app.Window must be unique and you should call NewExplorer
+// once per new app.Window.
+//
+// It's mandatory to use Explorer.ListenEvents on the same *app.Window.
+func NewExplorer(w *app.Window) (e *Explorer) {
+	idb, _ := rand.Int(rand.Reader, big.NewInt(math.MaxInt32))
+
+	e = &Explorer{
+		explorer: newExplorer(w),
+		id:       int32(idb.Int64()),
+	}
+
+	active.Store(e.id, e.explorer)
+	runtime.SetFinalizer(e, func(e *Explorer) { active.Delete(e.id) })
+
+	return e
+}
 
 // ListenEvents must get all the events from Gio, in order to get the GioView. You must
 // include that function where you listen for Gio events.
@@ -28,11 +78,14 @@ var (
 // 				(( ... your code ...  ))
 // 		}
 // }
-func ListenEvents(event event.Event) {
-	listenEvents(event)
+func (e *Explorer) ListenEvents(evt event.Event) {
+	if e == nil {
+		return
+	}
+	e.listenEvents(evt)
 }
 
-// ReadFile shows the file selector, allowing the user to select a single file.
+// Import shows the file selector, allowing the user to select a single file.
 // Optionally, it's possible to define which file extensions is supported to
 // be selected (such as `.jpg`, `.png`).
 //
@@ -41,18 +94,71 @@ func ListenEvents(event event.Event) {
 //
 // In some platforms the resulting `io.ReadCloser` is a `os.File`, but it's not
 // a guarantee.
-func ReadFile(extensions ...string) (io.ReadCloser, error) {
-	return openFile(extensions...)
+//
+// It's a blocking call, you should call it on a separated goroutine. For most OSes, only one
+// Import or Export, can happen at the same time, for each app.Window/Explorer.
+func (e *Explorer) Import(extensions ...string) (io.ReadCloser, error) {
+	if e == nil {
+		return nil, ErrNotAvailable
+	}
+
+	if runtime.GOOS != "js" {
+		e.mutex.Lock()
+		defer e.mutex.Unlock()
+	}
+
+	return e.importFile(extensions...)
 }
 
-// WriteFile opens the file selector, and writes the given content into
+// Export opens the file selector, and writes the given content into
 // some file, which the use can choose the location.
 //
 // It's important to close the `io.WriteCloser`. In some platforms the
 // file will be saved only when the writer is closer.
 //
-// In some platforms the resulting `io.ReadCloser` is a `os.File`, but it's not
+// In some platforms the resulting `io.WriteCloser` is a `os.File`, but it's not
 // a guarantee.
+//
+// It's a blocking call, you should call it on a separated goroutine. For most OSes, only one
+// Import or Export, can happen at the same time, for each app.Window/Explorer.
+func (e *Explorer) Export(name string) (io.WriteCloser, error) {
+	if e == nil {
+		return nil, ErrNotAvailable
+	}
+
+	if runtime.GOOS != "js" {
+		e.mutex.Lock()
+		defer e.mutex.Unlock()
+	}
+
+	return e.exportFile(name)
+}
+
+var (
+	DefaultExplorer *Explorer
+)
+
+// ListenEventsWindow calls Explorer.ListenEvents on DefaultExplorer,
+// and creates a new Explorer, if needed.
+//
+// Deprecated: Use NewExplorer instead.
+func ListenEventsWindow(win *app.Window, event event.Event) {
+	if DefaultExplorer == nil {
+		DefaultExplorer = NewExplorer(win)
+	}
+	DefaultExplorer.ListenEvents(event)
+}
+
+// ReadFile calls Explorer.ImportFile on DefaultExplorer.
+//
+// Deprecated: Use NewExplorer and Explorer.Import instead.
+func ReadFile(extensions ...string) (io.ReadCloser, error) {
+	return DefaultExplorer.Import(extensions...)
+}
+
+// WriteFile calls Explorer.ExportFile on DefaultExplorer.
+//
+// Deprecated: Use NewExplorer and Explorer.Export instead.
 func WriteFile(name string) (io.WriteCloser, error) {
-	return createFile(name)
+	return DefaultExplorer.Export(name)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module gioui.org/x
 go 1.16
 
 require (
-	gioui.org v0.0.0-20211230092323-cce0a121e1b8
+	gioui.org v0.0.0-20220105104929-8d8aeef66bef
 	github.com/yuin/goldmark v1.4.0
 	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
