38c3558507b5ba17ec0b720fb054411fda528df7
Inkeliz <inkeliz@inkeliz.com>
2021-12-30 15:39:18 +0000

    explorer: [ios] move functions to .m file
    Signed-off-by: Inkeliz <inkeliz@inkeliz.com>

--- explorer/explorer_ios.m
+++ explorer/explorer_ios.m
@@ -8,61 +8,121 @@
 #import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 #include "_cgo_export.h"
 
-@implementation explorer
+@implementation explorer_picker
 - (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
-    NSURL * url = [urls objectAtIndex:0];
-    [url startAccessingSecurityScopedResource];
+    NSURL *url = [urls objectAtIndex:0];
 
-    if (self.mode == EXPORT_MODE) {
+    switch (self.mode) {
+    case EXPORT_MODE:
         exportCallback((__bridge_retained CFTypeRef)url, self.id);
-    } else {
+        return;
+    case IMPORT_MODE:
         importCallback((__bridge_retained CFTypeRef)url, self.id);
+        return;
     }
 }
 - (void)documentPickerWasCancelled:(UIDocumentPickerViewController *)controller {
-    if (self.mode == EXPORT_MODE) {
+    switch (self.mode) {
+    case EXPORT_MODE:
         exportCallback(0, self.id);
-    } else {
+        return;
+    case IMPORT_MODE:
         importCallback(0, self.id);
+        return;
     }
 }
-- (bool) show:(char*)text {
-    if (self.mode == EXPORT_MODE) {
-        return [self exportFile:text];
-    } else {
-        return [self importFile:text];
-    }
-}
-- (bool) exportFile:(char*)name {
-   if (@available(iOS 14, *)) {
-        self.picker = [[UIDocumentPickerViewController alloc] initForExportingURLs:@[[NSURL URLWithString:@(name)]] asCopy:true];
-        self.picker.delegate = self;
+@end
 
-        [self.controller presentViewController:self.picker animated:YES completion:nil];
+CFTypeRef createPicker(CFTypeRef controllerRef, int32_t id) {
+	explorer_picker *e = [[explorer_picker alloc] init];
+	e.controller = (__bridge UIViewController *)controllerRef;
+	e.id = id;
+	return (__bridge_retained CFTypeRef)e;
+}
+
+bool exportFile(CFTypeRef expl, char * name) {
+   if (@available(iOS 14, *)) {
+        explorer_picker *explorer = (__bridge explorer_picker *)expl;
+        explorer.picker = [[UIDocumentPickerViewController alloc] initForExportingURLs:@[[NSURL URLWithString:@(name)]] asCopy:true];
+        explorer.picker.delegate = explorer;
+        explorer.mode = EXPORT_MODE;
+
+        [explorer.controller presentViewController:explorer.picker animated:YES completion:nil];
         return YES;
     }
     return NO;
 }
-- (bool) importFile:(char*)ext {
-    if (@available(iOS 14, *)) {
+
+bool importFile(CFTypeRef expl, char * ext) {
+  if (@available(iOS 14, *)) {
+        explorer_picker *explorer = (__bridge explorer_picker *)expl;
+
         NSMutableArray<NSString*> *exts = [[@(ext) componentsSeparatedByString:@","] mutableCopy];
         NSMutableArray<UTType*> *contentTypes = [[NSMutableArray alloc]init];
 
         int i;
         for (i = 0; i < [exts count]; i++) {
-UTType * utt = [UTType typeWithFilenameExtension:exts[i]];
+            UTType *utt = [UTType typeWithFilenameExtension:exts[i]];
             if (utt != nil) {
                 [contentTypes addObject:utt];
             }
         }
 
-        self.picker = [[UIDocumentPickerViewController alloc] initForOpeningContentTypes:contentTypes asCopy:true];
-        self.picker.delegate = self;
+        explorer.picker = [[UIDocumentPickerViewController alloc] initForOpeningContentTypes:contentTypes asCopy:true];
+        explorer.picker.delegate = explorer;
+        explorer.mode = IMPORT_MODE;
 
-        [self.controller presentViewController:self.picker animated:YES completion:nil];
+        [explorer.controller presentViewController:explorer.picker animated:YES completion:nil];
         return YES;
     }
     return NO;
 }
-@end
 
+CFTypeRef fileWriteHandler(CFTypeRef u) {
+    NSURL *url = (__bridge NSURL *)u;
+    [url startAccessingSecurityScopedResource];
+
+	NSError *err = nil;
+	NSFileHandle *handler = [NSFileHandle fileHandleForWritingToURL:url error:&err];
+	if (err != nil) {
+		return 0;
+	}
+	return (__bridge_retained CFTypeRef)handler;
+}
+
+CFTypeRef fileReadHandler(CFTypeRef u) {
+    NSURL *url = (__bridge NSURL *)u;
+    [url startAccessingSecurityScopedResource];
+
+	NSError *err = nil;
+	NSFileHandle *handler = [NSFileHandle fileHandleForReadingFromURL:url error:&err];
+	if (err != nil) {
+		return 0;
+	}
+	return (__bridge_retained CFTypeRef)handler;
+}
+
+bool fileWrite(CFTypeRef handler, uint8_t *b, uint64_t len) {
+	if (@available(iOS 13, *)) {
+        NSData *data = [NSData dataWithBytes:b length:len];
+
+        NSError *err = nil;
+        return [(__bridge NSFileHandle *)handler writeData:data error:&err];
+	}
+	return NO;
+}
+
+uint64_t fileRead(CFTypeRef handler, uint8_t *b, uint64_t len) {
+	if (@available(iOS 13, *)) {
+	    NSError *err = nil;
+		NSData *data = [(__bridge NSFileHandle *)handler readDataUpToLength:len error:&err];
+		[data getBytes:b length:data.length];
+		return data.length;
+	}
+	return 0;
+}
+
+void closeFile(CFTypeRef handler, CFTypeRef u) {
+	[(__bridge NSURL *)u stopAccessingSecurityScopedResource];
+	[(__bridge NSFileHandle *)handler closeFile];
+}
