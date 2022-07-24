8415d02c9cf2a0bcf9a09b60e1cf19bd1a206436
Chris Waldon <christopher.waldon.dev@gmail.com>
2022-07-24 10:28:18 -0400

    outlay: add test for grid sizing
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- outlay/grid_test.go
+++ outlay/grid_test.go
@@ -61,3 +61,50 @@ func TestGridLockedRows(t *testing.T) {
 		t.Errorf("expected highest Y index laid out to be %d, got %d", 9, highestY)
 	}
 }
+
+func TestGridSize(t *testing.T) {
+	var grid Grid
+	var ops op.Ops
+	var gtx layout.Context = layout.Context{
+		Constraints: layout.Constraints{
+			Min: image.Pt(10, 10),
+			Max: image.Pt(1000, 1000),
+		},
+		Metric: unit.Metric{PxPerDp: 1, PxPerSp: 1},
+		Queue:  &router.Router{},
+		Now:    time.Time{},
+		Locale: system.Locale{},
+		Ops:    &ops,
+	}
+
+	dimensioner := func(axis layout.Axis, index, constraint int) int {
+		return 10
+	}
+	layoutCell := func(gtx layout.Context, x, y int) layout.Dimensions {
+		return layout.Dimensions{Size: image.Pt(10, 10)}
+	}
+
+	// Ensure the returned size is less than the maximum.
+	dims := grid.Layout(gtx, 10, 10, dimensioner, layoutCell)
+	expected := (layout.Dimensions{Size: image.Pt(100, 100)})
+	if dims != expected {
+		t.Errorf("expected size %#+v, got %#+v", expected, dims)
+	}
+
+	// Ensure returned size respects maximum.
+	gtx.Constraints.Max = image.Pt(50, 50)
+	dims = grid.Layout(gtx, 10, 10, dimensioner, layoutCell)
+	expected = (layout.Dimensions{Size: image.Pt(50, 50)})
+	if dims != expected {
+		t.Errorf("expected size %#+v, got %#+v", expected, dims)
+	}
+
+	// Ensure returned size respects minimum.
+	gtx.Constraints.Min = image.Pt(500, 500)
+	gtx.Constraints.Max = image.Pt(1000, 1000)
+	dims = grid.Layout(gtx, 10, 10, dimensioner, layoutCell)
+	expected = (layout.Dimensions{Size: image.Pt(500, 500)})
+	if dims != expected {
+		t.Errorf("expected size %#+v, got %#+v", expected, dims)
+	}
+}
