02a5084a3ebdf0ffc961794b94154abc258bdeaf
Chris Waldon <christopher.waldon.dev@gmail.com>
2022-08-03 16:24:24 -0400

    richtext: add test for nil state
    It should be valid to provide nil state for Text if no spans
    are interactive.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

---
+++ richtext/richtext_test.go
@@ -0,0 +1,44 @@
+package richtext
+
+import (
+	"image"
+	"testing"
+	"time"
+
+	"gioui.org/font/gofont"
+	"gioui.org/io/router"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+)
+
+// TestNilInteractiveText ensures that it is safe to lay out
+// richtext with a nil state when none of the spans are
+// interactive.
+func TestNilInteractiveText(t *testing.T) {
+	th := material.NewTheme(gofont.Collection())
+	spans := []SpanStyle{
+		{
+			Size:    12,
+			Content: "Hello",
+		},
+		{
+			Size:    12,
+			Content: "world",
+		},
+	}
+	var ops op.Ops
+	gtx := layout.Context{
+		Constraints: layout.Exact(image.Pt(100, 100)),
+		Metric: unit.Metric{
+			PxPerDp: 1,
+			PxPerSp: 1,
+		},
+		Queue: &router.Router{},
+		Now:   time.Now(),
+		Ops:   &ops,
+	}
+
+	Text(nil, th.Shaper, spans...).Layout(gtx)
+}
