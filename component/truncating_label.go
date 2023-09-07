101388983c883b9be5b7f16731c6e3ad73f5f5ca
Chris Waldon <christopher.waldon.dev@gmail.com>
2023-09-07 09:09:07 -0400

    component: deprecate TruncatingLabelStyle
    This type is no longer needed now that the core text widgets internally support truncation.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- component/truncating_label.go
+++ component/truncating_label.go
@@ -1,60 +1,20 @@
 package component
 
 import (
-	"image"
-
 	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/op/clip"
 	"gioui.org/widget/material"
 )
 
 // TruncatingLabelStyle is a type that forces a label to
 // fit on one line and adds a truncation indicator symbol
 // to the end of the line if the text has been truncated.
+//
+// Deprecated: You can set material.LabelStyle.MaxLines to achieve truncation
+// without this type. This type has been reimplemented to do that internally.
 type TruncatingLabelStyle material.LabelStyle
 
 // Layout renders the label into the provided context.
 func (t TruncatingLabelStyle) Layout(gtx layout.Context) layout.Dimensions {
-	originalMaxX := gtx.Constraints.Max.X
-	gtx.Constraints.Max.X *= 2
-	asLabel := material.LabelStyle(t)
-	asLabel.MaxLines = 1
-	macro := op.Record(gtx.Ops)
-	dimensions := asLabel.Layout(gtx)
-	labelOp := macro.Stop()
-	if dimensions.Size.X <= originalMaxX {
-		// No need to truncate
-		labelOp.Add(gtx.Ops)
-		return dimensions
-	}
-	gtx.Constraints.Max.X = originalMaxX
-	truncationIndicator := asLabel
-	truncationIndicator.Text = "…"
-	macro = op.Record(gtx.Ops)
-	gtx.Constraints.Min.X = 0
-	indDims := truncationIndicator.Layout(gtx)
-	indOp := macro.Stop()
-	maxX := gtx.Constraints.Max.X - indDims.Size.X
-	return layout.Flex{
-		Alignment: layout.Baseline,
-		Spacing:   layout.SpaceEnd,
-	}.Layout(gtx,
-		layout.Rigid(func(gtx C) D {
-			defer clip.Rect(image.Rectangle{
-				Max: image.Point{
-					Y: dimensions.Size.Y,
-					X: maxX,
-				},
-			}).Push(gtx.Ops).Pop()
-			labelOp.Add(gtx.Ops)
-			dims := dimensions
-			dims.Size.X = maxX
-			return dims
-		}),
-		layout.Rigid(func(gtx C) D {
-			indOp.Add(gtx.Ops)
-			return indDims
-		}),
-	)
+	t.MaxLines = 1
+	return t.Layout(gtx)
 }
